4c14ba4becf074569c954d825a8e05e2a581ae0d
Florin Oltean <florin555@gmail.com>
2016-12-02 11:51:48 +0100

    Implement ui asking for number of players.

--- lib/ui.rb
+++ lib/ui.rb
@@ -1,5 +1,31 @@
+require 'remedy'
+
 class UI
+  include Remedy
+
   def get_number_of_players
-    2
+    viewport = Viewport.new
+    interaction = Interaction.new
+
+    title = "Number of players"
+    players_count = 1
+
+    viewport.draw Content.new([title, players_count.to_s])
+
+    interaction.loop do |key|
+      if key.to_s == 'up'
+        players_count += 1
+      end
+      if key.to_s == 'down'
+        players_count = [1, players_count - 1].max
+      end
+      if key.to_s == 'control_m'
+        break
+      end
+
+      viewport.draw Content.new([title, players_count.to_s])
+    end
+
+    players_count
   end
 end
