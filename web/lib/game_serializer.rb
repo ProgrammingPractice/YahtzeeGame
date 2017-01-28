2167b60eaf60b999e8f501ff6d95317fa78cf629
Victor Brender <victor.brender@gmail.com>
2017-01-28 14:07:00 +0100

    Passing categories between rounds

--- web/lib/game_serializer.rb
+++ web/lib/game_serializer.rb
@@ -6,7 +6,8 @@ module GameSerializer
     game.players.each do |player|
       hash['players'] << {
         'name'  => player.name,
-        'score' => player.score
+        'score' => player.score,
+        'categories' => player.categories
       }
     end
     JSON.dump(hash)
@@ -17,6 +18,7 @@ module GameSerializer
     JSON.load(data).fetch('players').each do |hash|
       player = Player.new(hash.fetch('name'), nil)
       player.instance_variable_set(:@score, hash.fetch('score'))
+      player.instance_variable_set(:@categories, hash.fetch('categories'))
       players << player
     end
     Game.new(players)
