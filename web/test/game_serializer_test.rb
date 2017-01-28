25971160a1f9a408ddf442912872fb225c13c621
Florin Oltean <florin555@gmail.com>
2017-01-28 14:33:56 +0100

    Remove unused files.

--- web/test/game_serializer_test.rb
+++ web/test/game_serializer_test.rb
@@ -10,9 +10,11 @@ class GameSerializerTest < Minitest::Test
     orig_player1 = Player.new('Player 1', nil)
     orig_player1.instance_variable_set(:@score, 25)
     orig_player1.instance_variable_set(:@categories, ['yahtzee'])
+
     orig_player2 = Player.new('Player 2', nil)
     orig_player2.instance_variable_set(:@score, 30)
     orig_player2.instance_variable_set(:@categories, ['ones', 'threes'])
+
     orig_game = Game.new([orig_player1, orig_player2])
 
     data = GameSerializer.dump(orig_game)
@@ -20,10 +22,12 @@ class GameSerializerTest < Minitest::Test
 
     game = GameSerializer.load(data)
     assert_equal Game, game.class
+
     player1 = game.players[0]
     assert_equal 'Player 1', player1.name
     assert_equal 25, player1.score
     assert_equal ['yahtzee'], player1.categories
+
     player2 = game.players[1]
     assert_equal 'Player 2', player2.name
     assert_equal 30, player2.score
