135975187cffb94d94f1bd19227da5e2a2c93518
Florin Oltean <florin.oltean@metoda.com>
2017-02-19 11:20:03 +0100

    Extract methods for the steps in the game.

--- web/test/game_test.rb
+++ web/test/game_test.rb
@@ -13,56 +13,67 @@ class YahtzeeWebTest < Minitest::Test
     Capybara.app = application
   end
 
-  def the_dice_will_be(dice)
-    unless @dice_roller.empty?
-      raise "FakeDiceRoller is not empty but you are trying to add new values!"
-    end
-    @dice_roller.add_values(dice)
+  def test_complete_game
+    the_dice_will_be([1,2,3,4,5])
+    start_new_game_with_players(2)
+    assert has_content?('You rolled: [1, 2, 3, 4, 5]')
+
+    the_dice_will_be([1,1,1,1,1])
+    player_holds_dice_in_round('Player 1', [], 1)
+    assert has_content?('You rolled: [1, 1, 1, 1, 1]')
+
+    the_dice_will_be([1,2,3])
+    player_holds_dice_in_round('Player 1', [3,4], 2)
+    assert has_content?('You rolled: [1, 2, 3, 1, 1]')
+
+    the_dice_will_be([2,2,2,2,2])
+    player_selects_category('Player 1', 'chance')
+
+    assert has_content?('Player 1: 8 points')
+    assert has_content?('Player 2: 0 points')
   end
 
-  def test_complete_game
+  def start_new_game_with_players(count)
     visit '/new_game'
     assert_equal 200, status_code
     assert has_content?('Yahtzee')
     assert has_content?('Please enter number of players:')
 
-    the_dice_will_be([1,2,3,4,5])
-
-    select('2', from: 'players_count')
+    select(count.to_s, from: 'players_count')
     click_button('Start game')
     assert_equal 200, status_code
     assert has_content?('Player 1: 0 points')
     assert has_content?('Player 2: 0 points')
+  end
 
-    assert has_content?('Playing -> Player 1')
-    assert has_content?('You rolled: [1, 2, 3, 4, 5]')
-    assert has_content?('roll 1/3')
+  def player_holds_dice_in_round(player_name, positions_to_hold, round_number)
+    assert has_content?("Playing -> #{player_name}")
+    assert has_content?("roll #{round_number}/3")
 
-    the_dice_will_be([1,1,1,1,1])
+    positions_to_hold.each do |p|
+      check("checkbox_dice_#{p}")
+    end
 
     click_button('Submit')
 
     assert_equal 200, status_code
-    assert has_content?('roll 2/3')
+  end
 
-    the_dice_will_be([1,2,3])
-
-    check('checkbox_dice_3')
-    check('checkbox_dice_4')
-    click_button('Submit')
-
-    assert_equal 200, status_code
+  def player_selects_category(player_name, category)
+    assert has_content?("Playing -> #{player_name}")
     assert has_content?('roll 3/3')
-    assert has_content?('You rolled: [1, 2, 3, 1, 1]')
     refute has_content?('Select what to hold:')
 
-    the_dice_will_be([2,2,2,2,2])
-
-    choose('radiobutton_category_chance')
+    choose("radiobutton_category_#{category}")
     click_button('Select category')
 
     assert_equal 200, status_code
-    assert has_content?('Player 1: 8 points')
-    assert has_content?('Player 2: 0 points')
+  end
+
+  def the_dice_will_be(dice)
+    unless @dice_roller.empty?
+      raise "FakeDiceRoller is not empty but you are trying to add new values!"
+    end
+    @dice_roller.add_values(dice)
   end
 end
