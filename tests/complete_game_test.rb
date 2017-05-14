342e405debb7d4b303bc4dece0155bee28508f51
Florin Oltean <florin555@gmail.com>
2017-05-14 10:19:06 +0200

    Move extract_* methods into TestData.

--- tests/complete_game_test.rb
+++ tests/complete_game_test.rb
@@ -3,7 +3,6 @@ require 'json'
 
 class CompleteGameTest < Minitest::Test
   class FakeUI
-    attr_reader :current_round
     attr_reader :output
 
     def initialize(test, rounds, dice_roller)
@@ -48,13 +47,13 @@ class CompleteGameTest < Minitest::Test
     def start_of_player_turn(player)
       @current_round = @test_data.get_next_round(player)
       @dice_roller.move_to_next_round(@current_round)
-      @hold_positions = extract_hold_positions(*@current_round)
+      @hold_positions = @test_data.extract_hold_positions
     end
 
     def end_of_player_turn(player)
       @dice_roller.ensure_exact_use_of_dice
 
-      expected_score = extract_score(*@current_round)
+      expected_score = @test_data.extract_score
       @test.assert_equal expected_score, player.score
     end
 
@@ -67,23 +66,7 @@ class CompleteGameTest < Minitest::Test
     end
 
     def ask_for_category
-      extract_category(*@current_round)
-    end
-
-    private
-
-    def extract_hold_positions(roll0, hold0, roll1, hold1, roll2, category, score)
-      hold_positions0 = [0,1,2,3,4].select { |i| hold0[i] == 'x' }
-      hold_positions1 = [0,1,2,3,4].select { |i| hold1[i] == 'x' } unless hold1.empty?
-      [hold_positions0, hold_positions1]
-    end
-
-    def extract_category(roll0, hold0, roll1, hold1, roll2, category, score)
-      category
-    end
-
-    def extract_score(roll0, hold0, roll1, hold1, roll2, category, score)
-      score
+      @test_data.extract_category
     end
   end
 
@@ -95,7 +78,24 @@ class CompleteGameTest < Minitest::Test
     end
 
     def get_next_round(player)
-      @rounds_iterators[player.name].next
+      @current_round = @rounds_iterators[player.name].next
+    end
+
+    def extract_hold_positions
+      (roll0, hold0, roll1, hold1, roll2, category, score) = @current_round
+      hold_positions0 = [0,1,2,3,4].select { |i| hold0[i] == 'x' }
+      hold_positions1 = [0,1,2,3,4].select { |i| hold1[i] == 'x' } unless hold1.empty?
+      [hold_positions0, hold_positions1]
+    end
+
+    def extract_score
+      (roll0, hold0, roll1, hold1, roll2, category, score) = @current_round
+      score
+    end
+
+    def extract_category
+      (roll0, hold0, roll1, hold1, roll2, category, score) = @current_round
+      category
     end
   end
 
