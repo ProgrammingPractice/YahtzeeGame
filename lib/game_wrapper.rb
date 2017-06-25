baa15e702bf4b68f8669419ff933a9825ac716d0
Florin Oltean <florin555@gmail.com>
2017-06-25 10:23:08 +0200

    Use Objects for round steps.

--- lib/game_wrapper.rb
+++ lib/game_wrapper.rb
@@ -1,4 +1,6 @@
 class GameWrapper
+  class RoundStep < Struct.new(:action, :callback); end
+
   def initialize(game)
     @game = game
     advance_to_next_player
@@ -34,11 +36,11 @@ class GameWrapper
   end
 
   def next_step
-    steps[@current_step][0]
+    steps[@current_step].action
   end
 
   def advance(input_from_user)
-    callback = steps[@current_step][1]
+    callback = steps[@current_step].callback
     callback.call(input_from_user)
     advance_current_step(input_from_user)
   end
@@ -66,26 +68,26 @@ class GameWrapper
 
   def steps
     [
-      [
+      RoundStep.new(
         :ask_for_hold_positions,
         ->(hold_positions) do
           # FIXME: we should roll before asking the user for hold positions.
           @current_player.roll_dice
           @current_player.reroll(positions_to_reroll(hold_positions))
         end
-      ],
-      [
+      ),
+      RoundStep.new(
         :ask_for_hold_positions,
         ->(hold_positions) do
           @current_player.reroll(positions_to_reroll(hold_positions))
         end
-      ],
-      [
+      ),
+      RoundStep.new(
         :ask_for_category,
         ->(category) do
           @current_player.select_category(category)
         end
-      ],
+      ),
     ]
   end
 end
